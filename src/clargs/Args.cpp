c9272476f9e8af4dc0d0100e32763a714ac1744a
Ivan Gagis <igagis@gmail.com>
2018-05-29 10:59:15 +0300

    stuff

--- src/clargs/Args.cpp
+++ src/clargs/Args.cpp
@@ -2,6 +2,12 @@
 
 using namespace clargs;
 
-void Args::add(const std::string& shortKey, const std::string& longKey, const std::string& description, std::function<void()(std::string&)>& valueHandler) {
+void Args::add(
+		char shortKey,
+		const std::string& longKey,
+		const std::string& description,
+		std::function<void(std::string&&)>&& valueHandler
+	)
+{
 
 }
