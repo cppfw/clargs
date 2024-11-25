9a7396009b869130bc0238e0b933e7f6e80f36ee
Ivan Gagis <igagis@gmail.com>
2024-11-25 22:33:34 +0200

    vcpkg

---
+++ vcpkg/test/main.cpp
@@ -0,0 +1,11 @@
+#include <clargs/parser.hpp>
+
+int main(int argc, const char** argv){
+    clargs::parser p;
+
+    p.add('a', 'aaa', "bla bla", [](){});
+
+    std::cout << "desc = " << p.description() << std::endl;
+
+    return 0;
+}
