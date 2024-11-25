70c4ce0db88da00b30436c3c610b3c8c59de2a00
Ivan Gagis <igagis@gmail.com>
2024-11-25 22:35:01 +0200

    vcpkg: fix test

--- vcpkg/test/main.cpp
+++ vcpkg/test/main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, const char** argv){
     clargs::parser p;
 
-    p.add('a', 'aaa', "bla bla", [](){});
+    p.add('a', "aaa", "bla bla", [](){});
 
     std::cout << "desc = " << p.description() << std::endl;
 
